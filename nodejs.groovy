c307a02c31d01812db16de3eae36ad2b5b0be0d8
ktestdevops <53824245+ktestdevops@users.noreply.github.com>
2019-08-21 23:40:28 +0530

    change git repository user name

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     	scm {
-        	git('git://github.com/gvenkat09/nodejs-app.git') {  node -> // is hudson.plugins.git.GitSCM
+        	git('git://github.com/ktestdevops/nodejs-app.git') {  node -> // is hudson.plugins.git.GitSCM
             	node / gitConfigName('DSL User')
             	node / gitConfigEmail('jenkins-dsl@gmail.com')
         	}
